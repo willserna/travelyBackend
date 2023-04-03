706548f57d073542435157d80cdf5ced56957d11
William Serna <williamsernac@hotmail.com>
2023-04-03 10:27:06 -0500

    Swagger setup

---
+++ applications/app-service/src/main/java/co/com/travely/MainApplication.java
@@ -0,0 +1,15 @@
+package co.com.travely;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Reactive Travely API", version = "1.0", description = "Documentation APIs v1.0"))
+public class MainApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
