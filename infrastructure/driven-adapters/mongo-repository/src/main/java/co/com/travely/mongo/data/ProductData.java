abeff6c7182fdbaf3d4c94594b6bd266bb0b6930
William Serna <williamsernac@hotmail.com>
2023-04-04 19:17:51 -0500

    ProductData class implemented (Analog to entity)

---
+++ infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/travely/mongo/data/ProductData.java
@@ -0,0 +1,23 @@
+package co.com.travely.mongo.data;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Data
+@Document(collection = "products")
+@NoArgsConstructor
+public class ProductData {
+
+    @Id
+    private String id;
+    private String packageName;
+    private String details;
+    private String location;
+    private Integer stock;
+    private Date startDate;
+    private Integer persons;
+}
