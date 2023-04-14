cf78243b6bff9fd95d23fb5ef72b10565c7f391d
William Serna <williamsernac@hotmail.com>
2023-04-14 14:09:34 -0500

    CORS config

---
+++ applications/app-service/src/main/java/co/com/travely/config/WebConfig.java
@@ -0,0 +1,19 @@
+package co.com.travely.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class WebConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
